05516553d23db9817a66cdd487cae638a8032736
Haripriya <harry135789@gmail.com>
2024-09-27 12:19:02 +0530

    Updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,21 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './components/Home'
-import { Add } from './components/Add'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import  Add  from './components/Add'
+import Nav from './components/Nav'
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
 
-  const [count, setCount] = useState(0)
+function App() { const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>Hi Welcome here</h1>
-     <h3>Todays Components</h3>
-     <h4>React contents</h4>
-   <Home/>
-   <Add/>
+      {/* <h1>Hi Welcome here</h1>
+      <h3>Todays Components</h3>
+      <h4>React contents</h4> */}
+     <Nav />
+   
+      {/* <Add/> */}
+      <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/Add' element={<Add/>}></Route>
+      </Routes>
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
